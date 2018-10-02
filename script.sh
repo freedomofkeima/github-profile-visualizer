d1ba96a9f813672c628f5cad3037e74697ab5070
Iskandar Setiadi <iskandarsetiadi@gmail.com>
2018-10-02 17:39:21 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-# Get number of repositories
-num_repo=$(./ok.sh list_repos ${GITHUB_USER} | grep "full_name" | wc -l)
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-#Get number of followers
-num_followers=$(./ok.sh list_followers | grep "followers_url" | wc -l)
+pushd $CURRENT_DIR
+  # Get number of repositories
+  num_repo=$(./ok.sh list_repos ${GITHUB_USER} | grep "full_name" | wc -l)
 
-# Get number of following
-num_following=$(./ok.sh list_following | grep "following_url" | wc -l)
+  #Get number of followers
+  num_followers=$(./ok.sh list_followers | grep "followers_url" | wc -l)
 
-echo $num_repo
-echo $num_followers
-echo $num_following
+  # Get number of following
+  num_following=$(./ok.sh list_following | grep "following_url" | wc -l)
+
+  echo $num_repo
+  echo $num_followers
+  echo $num_following
+popd
+
