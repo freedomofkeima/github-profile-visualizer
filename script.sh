86feab8e401036baf38fecc3f13b9927a26ecc15
Iskandar Setiadi <iskandar.setiadi@hde.co.jp>
2018-10-03 14:53:10 +0900

    Use -j flag

--- script.sh
+++ script.sh
@@ -4,13 +4,13 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 pushd $CURRENT_DIR
   # Get number of repositories
-  num_repo=$(./ok.sh list_repos ${GITHUB_USER} | grep "full_name" | wc -l)
+  num_repo=$(./ok.sh -j list_repos ${GITHUB_USER} | grep "full_name" | wc -l)
 
   #Get number of followers
-  num_followers=$(./ok.sh list_followers | grep "followers_url" | wc -l)
+  num_followers=$(./ok.sh -j list_followers | grep "followers_url" | wc -l)
 
   # Get number of following
-  num_following=$(./ok.sh list_following | grep "following_url" | wc -l)
+  num_following=$(./ok.sh -j list_following | grep "following_url" | wc -l)
 
   echo $num_repo
   echo $num_followers
