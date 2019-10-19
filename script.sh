b3c1093a97735ac1f391a64d46a6e895d672ff6d
Iskandar Setiadi <iskandarsetiadi@gmail.com>
2019-10-19 05:44:30 +0000

    Add cron for counting total number of stars

--- script.sh
+++ script.sh
@@ -15,6 +15,14 @@ pushd $CURRENT_DIR
   # Get number of following
   num_following=$(./ok.sh -j list_following ${GITHUB_USER}  | grep "following_url" | wc -l | tr -d ' ')
 
+  # Get total number of stars
+  stars=$(./ok.sh -j list_repos ${GITHUB_USER} | grep -oP '"stargazers_count": \K([0-9]+)')
+
+  num_stars=0
+  for value in $stars; do
+    (( num_stars += value ))
+  done
+
   # Get total number of Github users
   num_github_users=$(curl -s https://api.github.com/search/users?q=followers:%3E=0 | jq -r '.total_count')
 
@@ -28,6 +36,7 @@ pushd $CURRENT_DIR
   echo "${CURRENT_DATE},${num_repo}" >> data/num_repo.txt
   echo "${CURRENT_DATE},${num_followers}" >> data/follower.txt
   echo "${CURRENT_DATE},${num_following}" >> data/following.txt
+  echo "${CURRENT_DATE},${num_stars}" >> data/num_stars.txt
   echo "${CURRENT_DATE},${num_github_users}" >> data/num_github_users.txt
   echo "${CURRENT_DATE},${num_github_users_followers}" >> data/num_github_users_followers.txt
   echo "${CURRENT_DATE},${num_github_users_repo}" >> data/num_github_users_repo.txt
